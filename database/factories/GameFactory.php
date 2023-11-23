b721ce6391006f25cd8d0a332f0b96993261a34f
Rosa <xenco34@gmail.com>
2023-11-23 11:56:26 +0100

    Add GameFactory

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
+ */
+class GameFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        
+        return [
+            'dice1' => 6,
+            'dice2' => 7,
+            'user_id' => User::factory(),
+            'won' => $this->faker->boolean,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
