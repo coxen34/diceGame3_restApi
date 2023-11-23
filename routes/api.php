fadaee48cd78f778f98a60b257a6e693a8c92aac
Rosa <xenco34@gmail.com>
2023-11-23 12:08:46 +0100

    Delete comments

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,8 @@ use Laravel\Passport\Passport;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GameController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
+
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth:api');
