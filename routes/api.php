39e6f647e02bfa13af3f1a9a1bdef69d0f17811c
Rosa <xenco34@gmail.com>
2023-11-27 20:31:01 +0100

    Group routes by role

--- routes/api.php
+++ routes/api.php
@@ -12,31 +12,24 @@ Route::post('/players', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::middleware('auth:api')->group(function () {
-    
+
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
-    Route::put('/players/{id}', [UserController::class, 'update'])->name('users.update')->middleware('role:admin');
-    Route::put('/players/{id}', [UserController::class, 'update'])->name('users.update')->middleware('role:player');
+    Route::middleware('role:admin')->group(function () {
 
-    //Tirada Daus
-    Route::post('/players/{id}/games', [GameController::class, 'throwDice'])->name('games.throwDice')->middleware('role:admin');
-    Route::post('/players/{id}/games', [GameController::class, 'throwDice'])->name('games.throwDice')->middleware('role:player');
+        Route::delete('/players/{id}/games', [GameController::class, 'delete'])->name('games.deletePlayerGames');
+        Route::get('/players', [UserController::class, 'index'])->name('users.index');
+        Route::get('/players/ranking', [UserController::class, 'getPlayersRanking'])->name('players.ranking');
+        Route::get('/players/ranking/loser', [UserController::class, 'getWorstPlayer'])->name('players.ranking/loser');
+        Route::get('/players/ranking/winner', [UserController::class, 'getBestPlayer'])->name('players.ranking/winner');
+    });
 
-    //Elimina les tirades del jugador/a
-    Route::delete('/players/{id}/games', [GameController::class, 'delete'])->name('games.deletePlayerGames')->middleware('role:admin');
-    Route::delete('/players/{id}/games', [GameController::class, 'delete'])->name('games.deletePlayerGames')->middleware('role:player');
+    Route::middleware('role:player')->group(function () {
 
-    // Llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits
-    Route::get('/players', [UserController::class, 'index'])->name('users.index')->middleware('role:admin');
-
-    //Llistat jugades X jugador ID
-    Route::get('/players/{id}/games', [GameController::class, 'getPlayerGames'])->name('games.getPlayerGames')->middleware('role:admin');
-    Route::get('/players/{id}/games', [GameController::class, 'getPlayerGames'])->name('games.getPlayerGames')->middleware('role:player');
-
-    //Rànquing mitjà de tots els jugadors/es del sistema.
-    Route::get('/players/ranking', [UserController::class, 'getPlayersRanking'])->name('players.ranking')->middleware('role:admin');
-
-    Route::get('/players/ranking/loser', [UserController::class, 'getWorstPlayer'])->name('players.ranking/loser')->middleware('role:admin');
-
-    Route::get('/players/ranking/winner', [UserController::class, 'getBestPlayer'])->name('players.ranking/winner')->middleware('role:admin');
+        Route::put('/players/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::post('/players/{id}/games', [GameController::class, 'throwDice'])->name('games.throwDice');
+        Route::get('/players/{id}/games', [GameController::class, 'getPlayerGames'])->name('games.getPlayerGames');
+    });
 });
+
+
