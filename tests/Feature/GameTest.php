8a1e1c56e89a3ec64d32768a758566e494f27c76
Rosa <xenco34@gmail.com>
2023-11-28 20:52:41 +0100

    tests OK

--- tests/Feature/GameTest.php
+++ tests/Feature/GameTest.php
@@ -14,105 +14,93 @@ class GameTest extends TestCase
 {
     // use RefreshDatabase;
     use DatabaseTransactions;
-    
-//------------------FUNCIONES HELPER------------
-private function getUser($name)
-{
-   $user = User::where('name', $name)->first();
-   if (!$user) {
-       $user = User::factory()->create(['name' => $name]);
-   }
-   return $user;
-}
-private function assertResponseThrowDice($response)
-{
-   if ($response->status() === 302) {
-       $response->assertRedirect('/api/login'); 
-   } else {
-       $response->assertStatus(200)
-           ->assertJson([
-               'message' => 'Has tirado los dados!',
-           ]);
-   }
-}
-private function assertResponseDelete($response)
-{
- if ($response->status() === 302) {
-    $response->assertRedirect('/api/login'); 
- } else {
-    $response->assertStatus(200)
-        ->assertJson([
-            'message' => 'Tiradas del jugador/a eliminadas correctamente',
-        ]);
- }
-}
-//-----------------FIN FUNCIONES HELPER------------
-public function test_throw_dice()
-{
-   $user = $this->getUser('player');
-   $this->actingAs($user);
 
-   $targetUser = $this->getUser('Carla');
+    //------------------FUNCIONES HELPER------------
+    private function getUser($name)
+    {
+        $user = User::where('name', $name)->first();
+        if (!$user) {
+            $user = User::factory()->create(['name' => $name]);
+        }
+        return $user;
+    }
+    private function assertResponseThrowDice($response)
+    {
+        if ($response->status() === 302) {
+            $response->assertRedirect('/api/login');
+        } else {
+            $response->assertStatus(200)
+                ->assertJson([
+                    'message' => 'Has tirado los dados!',
+                ]);
+        }
+    }
+    private function assertResponseDelete($response)
+    {
+        if ($response->status() === 302) {
+            $response->assertRedirect('/api/login');
+        } else {
+            $response->assertStatus(200)
+                ->assertJson([
+                    'message' => 'Tiradas del jugador/a eliminadas correctamente',
+                ]);
+        }
+    }
+    //-----------------FIN FUNCIONES HELPER------------
+    public function test_throw_dice()
+    {
+        $user = $this->getUser('player');
+        $this->actingAs($user);
 
-   $response = $this->post('/api/players/' . $targetUser->id . '/games');
+        $targetUser = $this->getUser('Carla');
 
-   $this->assertResponseThrowDice($response);
-}
+        $response = $this->post('/api/players/' . $targetUser->id . '/games');
 
-
-public function test_delete()
-{
- $user = $this->getUser('player');
- $this->actingAs($user);
-
- $targetUser = $this->getUser('Carla');
-
- 
- $game = Game::create(['user_id' => $targetUser->id,'dice1' => 1,
- 'dice2' => 2]);
+        $this->assertResponseThrowDice($response);
+    }
 
 
- $token = $user->createToken('TestToken')->accessToken;
+    public function test_delete()
+    {
+        $user = User::factory()->create();
+
+        $user->assignRole('admin');
+
+        Game::factory()->count(3)->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user, 'api')->delete("/api/players/{$user->id}/games");
+
+        $this->assertResponseDelete($response);
+    }
 
 
- $response = $this->withToken($token)->delete('/api/players/' . $targetUser->id . '/games');
 
- 
- $this->assertResponseDelete($response);
+    public function test_getPlayerGames()
+    {
 
- 
- $this->assertDatabaseMissing('games', ['id' => $game->id]);
-}
+        $user = User::where('id', 1)->first();
+        
+        $user->assignRole('player');
+
+        $game = Game::where('user_id', $user->id)->first();
 
 
-public function test_getPlayerGames()
-{
- 
- $user = User::where('id', 1)->first();
+        $this->actingAs($user, 'api');
 
- 
- $game = Game::where('user_id', $user->id)->first();
 
- 
- $this->actingAs($user, 'api');
+        $response = $this->get('/api/players/' . $user->id . '/games');
 
- 
- $response = $this->get('/api/players/' . $user->id . '/games');
+        // Verifica la respuesta
+        $response->assertStatus(200)
+            ->assertJson([
+                'player_id' => $user->id,
+                'games' => [
+                    [
+                        'id' => $game->id,
+                        'user_id' => $user->id,
 
- // Verifica la respuesta
- $response->assertStatus(200)
-   ->assertJson([
-       'player_id' => $user->id,
-       'games' => [
-           [
-             'id' => $game->id,
-             'user_id' => $user->id,
-            
-           ],
-       ],
-   ]);
-}
-
-    
-    
+                    ],
+                ],
+            ]);
+    }
 }
